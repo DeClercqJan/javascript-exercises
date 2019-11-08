e42f3c68b5ffffab10d253eeac839a5024950bd7
jan <declercqjan@gmail.com>
2019-11-08 16:48:45 +0100

    struggling with async

--- 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/01-get-posts/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/01-get-posts/script.js
@@ -10,5 +10,45 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+  /* EERSTE POGING 
+    document.getElementById("run").addEventListener("click", function() {
+        // OPM: ik heb gewoon een error-message meegegven, en de console is leeg ALS JE OP DE KNOP DRUKT maar mogelijk is het niet dat wat gevraagd wordt
+        console.log(window.lib.getPosts("error"));
+    })
+*/
+  // jasper zijn oplossing
+  (() => {
+    const callback = (err, articles) => {
+      console.table(articles);
+    };
+
+    document.getElementById("run").addEventListener("click", () => {
+      window.lib.getPosts(callback);
+    });
+  })();
+  
 })();
+
+/*
+
+The <strong> window.lib.getPosts()</strong> function is available in the script. 
+It must receive a callback parameter. 
+After a short delay, the callback will be executed with two parameters: 
+the first contains an error (if there is one, otherwise null), the second an array of articles.
+
+DEZE VORM VERSTA IK NOG, MAAR ALS HET DAN IN ARROW FUNCTIONS WORDT GESCHREVEN + VERSCHILLENDE MAPPEN WPORDEN AANGEROEPEN
+    function createQuote(quote, callback){ 
+        var myQuote = "Like I always say, " + quote;
+        callback(myQuote); // 2
+      }
+      
+      function logQuote(quote){
+        console.log(quote);
+      }
+      
+      createQuote("eat your vegetables!", logQuote); // 1
+
+
+      
+*/
--- 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/02-get-comments/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/02-get-comments/script.js
@@ -10,5 +10,49 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+
+  // oplossing, geïnspireerd door Sander
+  // enkel posts oproepen en binnen posts roep je getComments aan met de info die je over de articles hebt verzameld
+
+  (() => {
+    const callback = (err, articles) => {
+      // console.table(articles);
+      console.log(articles);
+      const callback_2 = (err, comments) => {
+        console.log(comments);
+      };
+      // to do:   adds the comments obtained in a comments property of the item.
+      new_array = [];
+      new_array.push({ type: "Fiat", model: "500", color: "white" });
+      // console.log(new_array);
+      // console.log(new_array.type);
+      // console.log(new_array[0].type);
+      console.log(articles[0].title);
+      articles.forEach(element => {
+        // console.log(element);
+        // console.log(element.id);
+        // articles.push(element.id);
+        comment = window.lib.getComments(element.id, callback_2);
+        //  element.push(comment);
+        console.log(comment);
+        // element.join(comment);
+        // console.log(element);
+      });
+      // console.log(articles);
+    };
+    document.getElementById("run").addEventListener("click", () => {
+      window.lib.getPosts(callback);
+      // window.lib.getComments(1, callback);
+    });
+  })();
 })();
+
+/*
+You must have noticed, in the previous exercise, that the articles received via the window.lib.getPosts function included an id property. 
+The function window.lib.getComments() is available in the script. 
+It must receive an article id as the first parameter, as well as a callback as the second parameter. 
+After a short delay, the callback will be executed with two parameters: the first one contains an error (if there is one, if not null), the second one a table of the comments of the article. 
+When you click on the button, call the getPosts function. For each recovered item, calls the getComments function and adds the comments obtained in a comments property of the item. 
+Displays items in the console (getPosts and getComments functions never return errors)
+*/
