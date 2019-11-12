d8a549cb9e6dafe3e15cc9f1809fe8ce75caef89
jan <declercqjan@gmail.com>
2019-11-12 17:01:24 +0100

    saven op het einde van de dag

--- 1.JavaScript-Basics/2.Challenge-Series/07-misc/01-waving-text/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/07-misc/01-waving-text/script.js
@@ -9,41 +9,69 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+  (function() {
+    // HIERONDER DE OPLOSSING VAN SANDER -> NOG EENS BETER TE BEKIJKEN OM TE BEGRIJPEN EN EVENTUEEL MIJN EIGEN CODE NOG AAN TE PASSEN
+    //const maxFontsizeAddition = 100;
+    const minFontSizePixels = 12;
+    // const fontSizeSteps = (wordcount, maxFontsizeAddition) => maxFontsizeAddition/wordcount;
+     const highRes = true;
+     const separator = (highRes) ? "": " ";
+     
+     const textElement = document.getElementById("target");
+    
+     let textArr = textElement.innerText.split(separator);
+ 
+     let shift = 0;
+ 
+     setInterval(()=>{
+ 
+         textElement.innerHTML = textArr.map((word, index)=>{
+ 
+             if (++shift >= textArr.length/2) {
+                 shift = 0;
+             }
+             return `
+             <span style="font-size:${(shift + minFontSizePixels)}px">${word}</span>
+             `
+         }).join(separator);
+ 
+     }, 100);
 
-    // your code here
-    // document.getElementById("target").style.fontSize = "5em";
-    text = document.getElementById("target").innerHTML;
-    // console.log(text);
-    // console.log(typeof text);
-    // console.log(text.charAt(4));
-    text_appended="";
-    for (i=0; i<text.length; i ++) {
-        // console.log(i);
-        // console.log(text[i]);
-        // console.log(`<span style='color:red;>'${text[i]} </span>`);
-        text_appended += `<span style='color:red';>${text[i]}</span>`;
-        /*
-        var html = text[i].innerHTML;
-        text[i].innerHTML = html.substr(0, html.length-2)
-        + "<span color='red'>"
-        + text[i].innerHTML.substr(-2)
-        + "</span>";
-        */
+
+
+  /* EERSTE POGING, BENADERT HET RESULTAAT, MAAR IS NIET GOED
+  // your code here
+  // document.getElementById("target").style.fontSize = "5em";
+  text = document.getElementById("target").innerHTML;
+  setTimeout(function() {
+    for (i = 0; i < text.length; i++) {
+      // console.log(text[i]);
+      letter_appended = text.replace(
+        text[i],
+        `<span style='font-size:5em;'>${text[i]}</span>`
+      );
+      console.log(letter_appended);
+      document.getElementById("target").innerHTML = letter_appended;
     }
-document.getElementById("target").innerHTML = text_appended;
+  }, 1000);
+  */
 
-    /* text_array = text.split('');
-    console.log(text_array);
-    text_array.forEach(element => {
-        console.log(element);
-        // console.log(typeof element);
-    });
-    text_array.map(function(letter) {
-console.log(letter)
-    })
-    */
-
+  // console.log(text);
+  // console.log(typeof text);
+  // console.log(text.charAt(4));
+  /* DIT WERKT VOOR DE HELE TEKST, MAAR GAAT NIET 1 VOOR 1 
+  text_appended = "";
+  setTimeout(append_text, 2000);
+  function append_text() {
+    console.log("test");
+    for (i = 0; i < text.length; i++) {
+      // console.log(i);
+      // console.log(text[i]);
+      text_appended += `<span style='font-size:5em;'>${text[i]}</span>`;
+      document.getElementById("target").innerHTML = text_appended;
+    }
+  }
+      */
 })();
 
 /*
