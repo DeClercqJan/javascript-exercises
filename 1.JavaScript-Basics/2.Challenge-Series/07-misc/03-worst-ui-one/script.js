8456ed8b930170341183ff1b1f2ae06f6099c2e9
jan <declercqjan@gmail.com>
2019-11-13 17:00:30 +0100

    bezig met misc 7 oefening 4

--- 1.JavaScript-Basics/2.Challenge-Series/07-misc/03-worst-ui-one/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/07-misc/03-worst-ui-one/script.js
@@ -10,7 +10,27 @@
 // You will have time to focus on it later.
 
 (function() {
-
+    document.getElementById("slider").addEventListener("input", function() {
+        console.log("test");
+        value = document.getElementById("slider").value;
+        console.log(value);
+        document.getElementById("target").innerHTML = `0${value}`;
+    })
     // your code here
 
 })();
+
+/*
+            <section class="explain">
+                <p>An exercise in creating the worst possible user interface for dialing a phone number: use the value
+                    of the slider to show the phone number (with a leading 0) in the target paragraph (from 0460000000
+                    to 0499999999).</p>
+            </section>
+            <section class="material">
+                <p id="target">0460000000</p>
+                <form action="#" method="post">
+                    <div class="field full">
+                        <input type="range" min="460000000" max="499999999" value="460000000" id="slider"
+                            name="slider" />
+                    </div>
+*/
--- 1.JavaScript-Basics/2.Challenge-Series/07-misc/04-worst-ui-two/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/07-misc/04-worst-ui-two/script.js
@@ -10,7 +10,62 @@
 // You will have time to focus on it later.
 
 (function() {
+  button_html_collection = document.getElementsByTagName("button");
+  console.log(button_html_collection);
+  button_array = Array.from(button_html_collection);
+  console.log(button_array);
+  // HIERONDER EENS SNEL BEGINNEN SCHRIJVEN VOOR ALS IK HET AFZONDERLIJK VOOR ELK ZOU DOEN
+  part_one_html = document.getElementById("part-one").innerHTML;
+  part_one_html_min =  document.getElementById("part-one").getAttribute("data-min");
+  console.log(part_one_html);
+  console.log(part_one_html_min);
+  value = 0;
+  target = document.getElementById("target").innerHTML
+  console.log(target);
+i = 0;
+  document.getElementById("part-one").addEventListener("click", function() {
+      i++;
+      part_one_html_value= parseInt(part_one_html_min)+i;
+      console.log(part_one_html_value);
+      document.getElementById("part-one").innerHTML = part_one_html_value;
+      document.getElementById("target").innerHTML = `${part_one_html_value}`;   
+  })
+  
+  // document.getElementById("part-one").innerHTML = "test";
+  /* button_array.forEach(function(element) {
+    console.log(element);
+    console.log(element.id);
+    // IETS OM VERDER TE BEKIJKEN?
+    switch(element.id) {
+        case "part-one": console.log("test part One");
+        break;
 
-    // your code here
+    }
+    // DIT HIERONDER GAT NERGES NAARTOE
+    element.addEventListener("click", function (element_2) {
+        console.log(element_2);
+        console.log(typeof element_2);
+    })
+    element_data_min = element.getAttribute("data-min");
+    console.log(element_data_min);
 
+  });
+  */
+  // your code here
 })();
+
+/*
+<section class="explain">
+                <p>Another exercise in creating the worst possible user interface for dialing a phone number: every
+                    click increases the selected number. Minimum and maximum values are as specified on each button by
+                    the <em>data-min</em> and <em>data-max</em> attributes. Display the number (with a leading 0) in the
+                    target paragraph.</p>
+            </section>
+            <section class="material">
+                <p id="target">+460000000</p>
+                <div class="actions">
+                    <button type="button" id="part-one" data-min="460" data-max="499">460</button>
+                    <button type="button" id="part-two" data-min="00" data-max="99">00</button>
+                    <button type="button" id="part-three" data-min="00" data-max="99">00</button>
+                    <button type="button" id="part-four" data-min="00" data-max="99">00</button>
+*/
