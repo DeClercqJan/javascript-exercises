ebd5710014f5811b56f9b17ec05652febfe8227f
jan <declercqjan@gmail.com>
2019-11-13 15:14:57 +0100

    saven voor de pauze

--- 1.JavaScript-Basics/2.Challenge-Series/07-misc/02-typewriter-effect/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/07-misc/02-typewriter-effect/script.js
@@ -11,6 +11,40 @@
 
 (function() {
 
-    // your code here
+  // your code here
+  text = document.getElementById("target").innerHTML;
+  document.getElementById("target").innerHTML = "";
+  // console.log(text);
+  characters = text.split("");
+  // console.log(characters);
+  // console.log(typeof characters);
+  characters_length = characters.length;
+  // console.log(characters_length);
+  counter = 0;
+  // stuk hieronder werkt in console, maar ik krijg er de delay niet op
+
+  typed_array = [];
+  typed_string = [];
+  for (i = 0; i < characters.length; i++) {
+    console.log(characters[i]);
+    /*
+    typed_array.push(characters[i]);
+    console.log(typed_array);
+    typed_string = typed_array.join("");
+    console.log(typed_string);*/
+        document.getElementById("target").innerHTML += characters[i];
+  }
 
 })();
+
+/*
+      <p>Display some text inside the target element. Create a "typewriter" effect by making each letter
+                    appear one after the other. Use a random delay between each letter appearing.</p>
+            </section>
+            <section class="material">
+                <p id="target">Je suis un texte qui va apparaître dans un effet "machine à écrire", un peu comme dans
+                    les films noirs. Sauf que... je ne sais pas qui est l'assassin ! Y en a-t-il vraiment un ?
+                    Habite-t-il au 21 ? Que de mystères !</p>
+
+
+*/
