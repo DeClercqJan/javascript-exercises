64f6e5f27f9ba3340619eaf515ec74ffc77fe97c
jan <declercqjan@gmail.com>
2019-11-15 09:44:40 +0100

    ook misc 7 oef 7 gedaan

--- 1.JavaScript-Basics/2.Challenge-Series/07-misc/07-storage-clicker/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/07-misc/07-storage-clicker/script.js
@@ -12,5 +12,29 @@
 (function() {
 
     // your code here
-
+    document.getElementById("target").innerHTML = localStorage.getItem("number");
+    document.getElementById("increment").addEventListener("click", function() {
+        // counter = localStorage.setItem('key', 'value');   
+        // console.log(localStorage.getItem('key'));
+        counter = localStorage.getItem("number");
+        counter++
+        console.log(counter);
+        counter = localStorage.setItem("number", counter);
+        document.getElementById("target").innerHTML = localStorage.getItem("number");
+    })
 })();
+
+/*
+        <div class="content">
+            <section class="explain">
+                <p>When you click the button, increment the counter by 1 and display the new value in the target
+                    paragraph. Use <strong>localStorage</strong> to save the counter, and to read the counter value (and
+                    display it) when the page is reloaded.</p>
+            </section>
+            <section class="material">
+                <p id="target" class="counter">0</p>
+                <div class="actions">
+                    <button type="button" id="increment">Increment and store</button>
+                </div>
+
+*/
