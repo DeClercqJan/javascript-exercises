db969587c07406994b7c20a4aa0b55da849c8c7d
jan <declercqjan@gmail.com>
2019-11-15 16:19:58 +0100

    bezig met fetch. asynch overgeslagen. TO DO thuis

--- 1.JavaScript-Basics/2.Challenge-Series/09-fetch/01-list-to-console/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/09-fetch/01-list-to-console/script.js
@@ -10,5 +10,57 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+
+// PROBLEMEN OM SERVER OP TE ZETTEN
+// JEROEN HEEFT HET MET FETCH OPGELOST (ZONDER INSTALLATIE VAN SERVER)
+// JEROEN: HET IS ASYNC: JE HEBT EEN DERGELIJKE FUNCTIE NODIG
+// JEROEN HEEFT BTW OOK EEN PLUGIN ASYNCH GEINSTALLEERD
+// OPGELET: MOGEN GEEN PUNTKOMMA'S INSTAAN!
+/*
+async function test() {
+    await fetch("../../_shared/api.json")
+    .then(ste => ste.json())
+    .then(result => {
+        console.log(result)
+        console.log(result.heroes)
+    })
+}
+document.getElementById("run").addEventListener("click",test)
+*/
+
+// MET LOKALE SERVER
+async function test() {
+    await fetch("http://localhost:3000/heroes")
+    .then(ste => ste.json())
+    .then(result => {
+        console.log(result)
+    })
+}
+document.getElementById("run").addEventListener("click",test)
+    /*
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+           document.getElementById("demo").innerHTML = this.responseText;
+          }
+        };
+        xhttp.open("GET", "ajax_info.txt", true);
+        xhttp.send();
+      }
+      */
 })();
+
+/*
+edit: check readme.md bij hele groep oefeningen. daar staat wat in van nodejs en seve
+
+        <div class="content">
+            <section class="explain">
+                <p>Once the REST API of the exercise has been launched you can access it at the same domainname:port that your server resides on. This small API follows the REST precepts, and contains, under the /heroes path, data about X-Men. When you click on the
+                    button, use the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">fetch function</a> and following the REST architecture, retrieve the list of X-Men and display it in the console.</p>
+
+                <p>The technique to fetch data from the server with JavaScript is commonly called <a href="https://www.w3schools.com/js/js_ajax_intro.asp">Ajax</a>. While there are different syntaxes to make the call, the general theory stays the same.</p>
+                <p>The most modern approach without using a framework is the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">fetch function</a>, I recommend using it.</p>
+            </section>
+
+            */
--- 1.JavaScript-Basics/2.Challenge-Series/09-fetch/02-list-to-template/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/09-fetch/02-list-to-template/script.js
@@ -10,5 +10,53 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+
+  async function change() {
+    await fetch("http://localhost:3000/heroes")
+      .then(ste => ste.json())
+      .then(result => {
+        console.log(result);
+        x_men = result;
+        // DEZE WERKEN NIET OMDAT JE NIET VAN ARRAY PAKT, MAAR VAN JSONBESTAND
+        // console.log(result.name)
+        // console.log(result.alterEgo)
+        // console.log(result.abilities)
+        console.log(x_men);
+        x_men.forEach(element => {
+          console.log(element);
+          console.log(element.name);
+          console.log(element.alterEgo);
+          console.log(element.abilities);
+          element.abilities.forEach(element => {
+            console.log(element);
+          });
+        });
+      });
+  }
+  document.getElementById("run").addEventListener("click", change);
+  console.log(document.getElementById("hero"));
+  // er zal nog iets met innerHTML moeten gebeuren
 })();
+
+/*
+
+<p>When you click on the button, use the fetch function, retrieve the list of X-Men and display it in the tag whose id is "target". Use the template tag to reproduce an html structure for each hero.</p>
+<p>The template tag already exists in the HTML structure, be sure to check the HTML code!</p>
+</section>
+            <section class="material">
+                <div class="actions">
+                    <button type="button" id="run">Run the script</button>
+                </div>
+                <template id="tpl-hero">
+                        <li class="hero">
+                            <h4 class="title">
+                                <strong class="name"></strong>
+                                <em class="alter-ego"></em>
+                            </h4>
+                            <p class="powers"></p>
+                        </li>
+                    </template>
+                <ol id="target"></ol>I
+
+*/
