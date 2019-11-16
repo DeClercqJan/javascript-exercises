0241a0de686f3f971c3bdffbd06c6a4162c6c61d
jan <declercqjan@gmail.com>
2019-11-16 17:43:36 +0100

    fetch 09 oef 03 af

--- 1.JavaScript-Basics/2.Challenge-Series/09-fetch/03-details/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/09-fetch/03-details/script.js
@@ -10,5 +10,51 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+  // your code here
+
+  async function getID() {
+    id_entered = document.getElementById("hero-id").value;
+    console.log(id_entered);
+    await fetch("http://localhost:3000/heroes")
+      // .then(response => console.log(response))
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        result.forEach(element => {
+            console.log(element);
+            console.log(element.id);
+            console.log(element.name);  
+            if (element.id == id_entered) {document.getElementById("target").innerHTML = element.name}
+        });
+      });
+  }
+
+  document.getElementById("run").addEventListener("click", getID);
 })();
+
+/*
+            <section class="explain">
+                <p>When you click on the button, get the id from the form, then get the corresponding X-Men from the API and display it in the tag whose id is "target". Use the tag <strong>template</strong> to reproduce a suitable html structure.</p>
+            </section>
+            <section class="material">
+                <form action="#" method="post">
+                    <div class="field">
+                        <label for="hero-id">Id of the hero:</label>
+                        <input type="text" name="hero-id" id="hero-id" />
+                    </div>
+                    <div class="actions">
+                        <button type="button" id="run">Run the script</button>
+                    </div>
+                </form>
+                <template id="tpl-hero">
+                        <li class="hero">
+                            <h4 class="title">
+                                <strong class="name"></strong>
+                                <em class="alter-ego"></em>
+                            </h4>
+                            <p class="powers"></p>
+                        </li>
+                    </template>
+                <ol id="target"></ol>
+            </section>
+*/
