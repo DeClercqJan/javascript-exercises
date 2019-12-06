a33411c90216453c1b4479df3cfdfd50ad974d0a
jan <declercqjan@gmail.com>
2019-12-06 20:31:37 +0100

    2. chall 8 async oefening 9 klaar! ging deze keer gemakkelijker na wat opzoekwerk/basis info

--- 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/09-async-handling-errors/script.js
+++ 1.JavaScript-Basics/2.Challenge-Series/08-asynchronous/09-async-handling-errors/script.js
@@ -10,5 +10,20 @@
 // You will have time to focus on it later.
 
 (() => {
+    document.querySelector("#run").addEventListener("click", async function () {
+        try {
+               persons = await window.lib.getPersons();
+                console.log(persons);
+            }
+        catch {
+            console.error("foutje!");
+        }
+    })
     // your code here
 })();
+
+/*
+ The function window.lib.getPersons() is available in the script. It returns a Promise that will be resolved, after a short delay, with a table of people.
+<br />When you click on the button, call the getPersons function and display the result obtained in the console. Warning: the Promise returned by the getPersons function has more or less a half chance of being rejected with an error. If so, use console.error() to display it.
+<strong>Note :</strong> for this exercise, use the syntax <em>async/await</em>.
+*/
